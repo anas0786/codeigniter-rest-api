71f396324faa82ff2771cb56006de212e20d263c
anas0786 <anas.ansari992@gmail.com>
2021-04-24 17:03:41 +0530

    methods added in controller

--- application/controllers/api/Student.php
+++ application/controllers/api/Student.php
@@ -1,1 +1,22 @@
-
+<?php
+require APPPATH.'/libraries/REST_Controller.php';
+class Student extends REST_Controller{
+  
+  public function index_post()
+  {
+    echo"this is post method";
+  }
+  public function index_put()
+  {
+    echo"this is put method";
+  }
+  public function index_delete()
+  {
+    echo"this is delete method";
+  }
+  public function index_get()
+  {
+    echo"this is get method";
+  }
+}
+?>
